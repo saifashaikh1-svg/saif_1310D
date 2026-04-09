6a6e85a26edd7d09571d51b24a5ec90a0975848d
saifashaikh1-svg <saif.a.shaikh1@gmail.com>
2026-04-08 22:33:21 -0500

    Create volume_of_sphere.py

---
+++ volume_of_sphere.py
@@ -0,0 +1,10 @@
+def calculate_volume_of_sphere(radius):
+    pi = 3.14
+    volume = (4/3) * pi * radius**3
+    return volume
+
+radius1 = 30
+print(f"Volume with radius {radius1}: {calculate_volume_of_sphere(radius1)}")
+
+radius2 = 40
+print(f"Volume with radius {radius2}: {calculate_volume_of_sphere(radius2)}")
